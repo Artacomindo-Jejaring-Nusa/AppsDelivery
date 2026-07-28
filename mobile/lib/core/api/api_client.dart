bfc1c269fffe3ecfdd6e0d0747811307d2c09b10
Ahmad-Rizki21 <ahmad2005rizki@gmail.com>
2026-07-28 13:52:38 +0700

    chore(mobile): set default API base URL to http://2.2.2.50:8080 for Wi-Fi LAN

--- mobile/lib/core/api/api_client.dart
+++ mobile/lib/core/api/api_client.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient {
-  static const String defaultBaseUrl = 'http://10.0.2.2:8080';
+  static const String defaultBaseUrl = 'http://2.2.2.50:8080';
   late Dio dio;
 
   ApiClient() {
